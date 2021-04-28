7fe5a7ff7ddd15cbfb9574c3cd69ed3d5e7db40e
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-04-28 15:29:48 +0100

    end of course 16

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,13 +2,30 @@ require_relative './bike.rb'
 
 class DockingStation
   attr_reader :bike
+  DEFAULT_CAPACITY = 20
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    return Bike.new
+  #  return Bike.new
+    fail "No bikes docked" if empty?
+    @bikes >> bikes
   end
 
   def dock(bike)
-    @bike = bike
+    fail "Docking station full" if full?
+      @bikes << bike
   end
+
+  private
+
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+  @bikes.empty?
+end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,28 +5,42 @@ describe DockingStation do
   expect(subject).to respond_to :release_bike
   end
 
-  it "gets bike and checks bike is working" do
-  bike = subject.release_bike
-  expect(bike).to be_working
-  end
-
   it "allows a bike to be docked" do
   expect(subject).to respond_to(:dock).with(1).argument
   end
 
-  it "allows you to view bikes" do
-  expect(subject).to respond_to :bike
+    # it "returns if a bike is docked" do
+    #   bike = Bike.new
+    # subject.dock(bike)
+    # expect(subject.bike).to eq(bike)
+    # end
+
+describe "#release_bike" do
+
+  # it "gets bike and checks bike is working" do
+  # bike = subject.release_bike
+  # expect(bike).to be_working
+  # end
+
+  it "raises an error when there are no more bikes available" do
+    expect { subject.release_bike }.to raise_error "No bikes docked"
   end
 
-  it "docks a bike" do
-  bike = Bike.new
-  expect(subject.dock(bike)).to eq(bike)
-  end
+describe "dock" do
+    # it "docks a bike" do
+    # bike = Bike.new
+    # expect(subject.dock(bike)).to eq(bike)
+    # end
 
-  it "returns if a bike is docked" do
+  it "raising an error when docking ststion is full" do
     bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bike).to eq(bike)
+    DockingStation::DEFAULT_CAPACITY.times do
+      subject.dock Bike.new
+    end
+    expect { subject.dock(bike) }.to raise_error "Docking station full"
   end
 
+
+end
+end
 end
